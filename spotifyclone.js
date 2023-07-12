f33134c38d4eb97be1c0f7eb8b14e3da9173afce
Gagan <gagank2199@gmail.com>
2023-07-12 18:19:11 +0530

    js song add

---
+++ spotifyclone.js
@@ -0,0 +1,17 @@
+console.log("hey listeners");
+
+let song= new Audio('1song.mp3')
+// song.play();
+
+songelement= document.getElementById('playicon')
+
+let songs=[
+    {songName: "Humare saath", filePath:'1song.mp3', coverPath:"ram"}
+    {songName: "2mp3", filePath:'2song.mp3', coverPath:"ram"}
+    {songName: "3mp3", filePath:'3song.mp3', coverPath:"ram"}
+    {songName: "4mp3", filePath:'4song.mp3', coverPath:"ram"}
+    {songName: "5mp3", filePath:'5song.mp3', coverPath:"ram"}
+    
+];
+
+
