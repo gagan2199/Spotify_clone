17bc1f53e462e54866b1804e43343a962bebd040
Gagan <gagank2199@gmail.com>
2023-07-12 19:30:51 +0530

    song is playing

--- spotifyclone.js
+++ spotifyclone.js
@@ -1,17 +1,27 @@
 console.log("hey listeners");
 
-let song= new Audio('1song.mp3')
-// song.play();
+let song1 = new Audio('1song.mp3');
+// song1.play();
 
-songelement= document.getElementById('playicon')
+// let songElement = document.getElementById('playicon');
 
-let songs=[
-    {songName: "Humare saath", filePath:'1song.mp3', coverPath:"ram"}
-    {songName: "2mp3", filePath:'2song.mp3', coverPath:"ram"}
-    {songName: "3mp3", filePath:'3song.mp3', coverPath:"ram"}
-    {songName: "4mp3", filePath:'4song.mp3', coverPath:"ram"}
-    {songName: "5mp3", filePath:'5song.mp3', coverPath:"ram"}
-    
+let songs = [
+    { songName: "1song", filePath: '1song.mp3', coverPath: "ram" },
+    { songName: "2mp3", filePath: 'Perfect.mp3', coverPath: "Perfect.jpg" },
+    { songName: "3mp3", filePath: 'Memories.mp3', coverPath: "Memories.jpg" },
+    { songName: "4mp3", filePath: 'Dope.mp3', coverPath: "ram" },
+    { songName: "5mp3", filePath: 'Let Me Love You.mp3', coverPath: "Let Me Love You.jpg" }
+
 ];
+
+
+    let playicon = document.getElementById('playicon');
+    playicon.addEventListener('click', playit);
+
+    function playit() {
+
+        let song1 = new Audio('1song.mp3');
+        return song1.play();
+    }
 
 
